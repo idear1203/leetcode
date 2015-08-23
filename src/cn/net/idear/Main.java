0dd86c4acf02ef1e436c121d3beaaf734cb6a3d1
IDEAR1 <wangdongwei1203@gmail.com>
2015-08-23 17:24:13 +0800

    initial commit

---
+++ src/cn/net/idear/Main.java
@@ -0,0 +1,16 @@
+package cn.net.idear;
+
+import org.junit.Test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+
+}
