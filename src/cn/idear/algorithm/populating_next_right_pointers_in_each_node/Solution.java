c5588e93a21a1357c930d202e2c2ffcb1fc90da2
IDEAR1 <wangdongwei1203@gmail.com>
2015-09-25 21:38:30 +0800

    populating next right pointers in each node

---
+++ src/cn/idear/algorithm/populating_next_right_pointers_in_each_node/Solution.java
@@ -0,0 +1,44 @@
+package cn.idear.algorithm.populating_next_right_pointers_in_each_node;
+
+import cn.idear.algorithm.util.TreeLinkNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Created by wangdongwei on 9/25/15.
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null)
+            return;
+        if(root.left != null){
+            connect(root.left);
+            connect(root.right);
+            for(TreeLinkNode l = root.left, r= root.right; l != null ; l = l.right, r = r.left)
+                l.next = r;
+        }
+    }
+
+    public void connect1(TreeLinkNode root) {
+        if(root == null)
+            return;
+        Queue<TreeLinkNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeLinkNode dummy = new TreeLinkNode(0);
+        while(!queue.isEmpty()){
+            int len = queue.size();
+            TreeLinkNode cur = dummy;
+            for(int i = 0; i < len; i++){
+                TreeLinkNode node = queue.poll();
+                cur.next = node;
+                cur = cur.next;
+                if(node.left != null)
+                    queue.add(node.left);
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+        }
+    }
+}
---
+++ src/cn/idear/algorithm/util/TreeLinkNode.java
@@ -0,0 +1,11 @@
+package cn.idear.algorithm.util;
+
+/**
+ * Created by wangdongwei on 9/25/15.
+ */
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+    public TreeLinkNode(int x) { val = x; }
+
+}
