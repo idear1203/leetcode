316af6c69d23589a8cd57eb2aef24798aaeb9abb
IDEAR1 <wangdongwei1203@gmail.com>
2015-09-27 15:48:40 +0800

    triangle top-to-bottom method

--- src/cn/idear/algorithm/triangle/Solution.java
+++ src/cn/idear/algorithm/triangle/Solution.java
@@ -6,9 +6,33 @@ import java.util.List;
 /**
  * Created by wangdongwei on 9/26/15.
  */
-//TODO:效率极低
 public class Solution {
+    /**
+     * 状态转移方程：f(i, j ) = f(i, j) + min(f(i + 1, j), f(i + 1, j + 1))
+     * @param triangle
+     * @return
+     */
     public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle.isEmpty())
+            return 0;
+        int[] s = new int[triangle.size()];
+        for(int i = 0; i < triangle.size(); i++)
+            s[i] = triangle.get(triangle.size() - 1).get(i);
+        for(int i = triangle.size() - 2; i >= 0; i--)
+            for(int j = 0; j <= i; j++)
+                s[j] = Math.min(s[j], s[j + 1]) + triangle.get(i).get(j);
+//                triangle.get(i).set(j, triangle.get(i).get(j) +
+//                        Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
+//        return triangle.get(0).get(0);
+        return s[0];
+    }
+
+    /**
+     * 这个思路不错，但是由于是自顶向下的，因此最后需要一次O(n)的比较
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal1(List<List<Integer>> triangle) {
         if(triangle.isEmpty())
             return 0;
         int size = triangle.size();
