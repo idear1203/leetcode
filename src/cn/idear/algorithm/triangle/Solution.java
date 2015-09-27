fe74c42890fb47e0f6584816eec4f5b74dabe575
IDEAR1 <wangdongwei1203@gmail.com>
2015-09-27 15:49:05 +0800

    triangle top-to-bottom method

--- src/cn/idear/algorithm/triangle/Solution.java
+++ src/cn/idear/algorithm/triangle/Solution.java
@@ -21,9 +21,6 @@ public class Solution {
         for(int i = triangle.size() - 2; i >= 0; i--)
             for(int j = 0; j <= i; j++)
                 s[j] = Math.min(s[j], s[j + 1]) + triangle.get(i).get(j);
-//                triangle.get(i).set(j, triangle.get(i).get(j) +
-//                        Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
-//        return triangle.get(0).get(0);
         return s[0];
     }
 
